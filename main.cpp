00834fd8dca4aaa831a51f370ef90880a009f58d
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-11 12:20:46 -0700

    updated main

--- main.cpp
+++ main.cpp
@@ -9,11 +9,36 @@
 #include "op.hpp"
 #include "pow.hpp"
 
-
+/*
 int main() {
 
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* mult = new Mult(seven, four);
+    Base* add = new Add(three, mult);
+    Base* minus = new Sub(add, two);
 
+    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
 
 	return 0;
 
 }
+*/
+int main(int argc, char++ argv){
+	int i = 1;
+	while (i < argc) {
+	      cout << argv[i];
+	       i++;
+	}
+	cout << endl;
+	return 0;
+}	
+
+
+
+
+
+
+
