93ce98cc5d03e04472fa86370e29a94b2a580dfa
PatrickPicca <patrickpicca@gmail.com>
2021-05-11 12:26:53 -0700

    Started on factory file

---
+++ factory.hpp
@@ -0,0 +1,56 @@
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
+
+
+#include <add.hpp>
+#include <base.hpp>
+#include <div.hpp>
+#include <mult.hpp>
+#include <op.hpp>
+#include <pow.hpp>
+#include <rand.hpp>
+#include <sub.hpp>
+
+#include <iostream>
+
+class factory
+{
+	public:
+		Base* parse(char** input, int length)
+		{
+			
+
+			{
+			if (input[i] == nullptr){
+				int num1 = stod(input[i++]);
+				left_op = new Op(num1);
+			}
+			if (input[i] == "+") {
+				int num2 = stod(input[++i]);
+				left_op = new Add(left_op, new Op(num));
+			}
+			else if(input[i] == "/") {
+				int num2 = stod(input[++i]);
+				left_op = new Div(left_op, new Op(num2));
+			}
+			else if(input[i]=="-") {
+				int num2 = stod(input[++i]);
+				left_op = new Sub)left_op, new Op(num2));
+			}
+			else if(input[i]=="*") {
+				int num2 = stod(input[++i]);
+				left_of = new Mult(Left_op), new Op(num2));
+			}
+			else{
+				cout << "ERROR!" << endl;
+				return nullptr;
+			}
+			i++;
+			}
+			return left_op;
+		}
+		
+};
+
+
+#endif
