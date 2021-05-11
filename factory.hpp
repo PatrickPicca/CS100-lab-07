9c23c13e87dd77ce61a90930293f60263ad2d92b
PatrickPicca <patrickpicca@gmail.com>
2021-05-11 12:42:59 -0700

    Expanded factory file

--- factory.hpp
+++ factory.hpp
@@ -19,33 +19,42 @@ class factory
 		Base* parse(char** input, int length)
 		{
 			
-
-			{
-			if (input[i] == nullptr){
-				int num1 = stod(input[i++]);
-				left_op = new Op(num1);
-			}
-			if (input[i] == "+") {
-				int num2 = stod(input[++i]);
-				left_op = new Add(left_op, new Op(num));
-			}
-			else if(input[i] == "/") {
-				int num2 = stod(input[++i]);
-				left_op = new Div(left_op, new Op(num2));
-			}
-			else if(input[i]=="-") {
-				int num2 = stod(input[++i]);
-				left_op = new Sub)left_op, new Op(num2));
-			}
-			else if(input[i]=="*") {
-				int num2 = stod(input[++i]);
-				left_of = new Mult(Left_op), new Op(num2));
-			}
-			else{
-				cout << "ERROR!" << endl;
-				return nullptr;
-			}
-			i++;
+				
+			Base* left_op = nullptr;
+			for (int i = 2, i < ; i++){
+				if (input[i] == nullptr){
+					int num1 = stod(input[i++]);
+					left_op = new Op(num1);
+				}
+				if (input[i] == "+") {
+					int num2 = stod(input[++i]);
+					left_op = new Add(left_op, new Op(num));
+				}
+				else if(input[i] == "/") {
+					int num2 = stod(input[++i]);
+					left_op = new Div(left_op, new Op(num2));
+				}
+				else if(input[i]=="-") {
+					int num2 = stod(input[++i]);
+					left_op = new Sub(left_op, new Op(num2));
+				}
+				else if(input[i]=="*") {
+					int testing = i+1;
+					if (input[testing] =="*")
+					{
+						int num2 = stod(input[++testing]);
+						left_op = new Pow(left_op, new Op(num2));	
+					}
+					else {
+						int num2 = stod(input[++i]);
+						left_of = new Mult(left_op, new Op(num2));	
+					}
+				}
+				else{
+					cout << "ERROR!" << endl;
+					return nullptr;
+				}
+				i++;
 			}
 			return left_op;
 		}
