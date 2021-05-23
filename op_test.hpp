fba095374d76521f8e5e074b7970cc53133e6f89
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-23 13:17:04 -0700

    added tests

--- op_test.hpp
+++ op_test.hpp
@@ -37,7 +37,7 @@ TEST(FactoryTest, Multiplying){
 TEST(FactoryTest, Dividing){
         Factory test;
         char *args[] = {"8", "/", "2"};
-        Base *op = test.parse(args, 3);
+        Base *Op = test.parse(args, 3);
         EXPECT_DOUBLE_EQ(Op->evaluate(), 4);
 }
 
@@ -51,21 +51,21 @@ TEST(FactoryTest, Pow){
 TEST(FactoryTest, AddSub) {
 	Factory test;
 	char *args[] = { "7", "+", "2", "-", "3"};
-	Base *op = test.parse(args, 5);
+	Base *Op = test.parse(args, 5);
 	EXPECT_DOUBLE_EQ(op->evaluate(), 6);
 }
 
 TEST(FactoryTest, SubAdd) {
         Factory test;
         char *args[] = { "10", "-", "4", "+", "2"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 8);
 }
 
 TEST(FactoryTest, SubMult) {
         Factory test;
         char *args[] = { "2", "-", "1", "*", "7"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 7);
 }
 
@@ -73,84 +73,84 @@ TEST(FactoryTest, SubMult) {
 TEST(FactoryTest, MultSub) {
         Factory test;
         char *args[] = { "12", "*", "2", "-", "10"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 14);
 }
 
 TEST(FactoryTest, AddMult) {
         Factory test;
         char *args[] = { "4", "+", "3", "*", "5"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 30);
 }
 
 TEST(FactoryTest, MultAdd) {
         Factory test;
         char *args[] = { "5", "*", "4", "+", "6"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 26);
 }
 
 TEST(FactoryTest, AddDiv) {
         Factory test;
         char *args[] = { "20", "+", "8", "/", "2"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 14);
 }
 
 TEST(FactoryTest, SubDiv) {
         Factory test;
         char *args[] = { "11", "-", "1", "/", "5"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 2);
 }
 
 TEST(FactoryTest, MultDiv) {
         Factory test;
         char *args[] = { "3", "*", "8", "/", "6"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 4);
 }
 
 TEST(FactoryTest, PowDiv) {
         Factory test;
         char *args[] = { "3", "**", "2", "/", "3"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 3);
 }
 
 TEST(FactoryTest, MultPow) {
         Factory test;
         char *args[] = { "5", "*", "1", "**", "2"};
-        Base *op = test.parse(args, 5);
+        Base *Op = test.parse(args, 5);
         EXPECT_DOUBLE_EQ(op->evaluate(), 25);
 }
 
 TEST(FactoryTest, AddingNeg){
         Factory test;
         char *args[] = {"-2", "+", "10"};
-        Base *op = test.parse(args, 3);
+        Base *Op = test.parse(args, 3);
         EXPECT_DOUBLE_EQ(op->evaluate(), 8);
 }
 
 TEST(FactoryTest, SubtractingNeg){
         Factory test;
         char *args[] = {"-4", "-", "-7"};
-        Base *op = test.parse(args, 3);
+        Base *Op = test.parse(args, 3);
         EXPECT_DOUBLE_EQ(op->evaluate(), 3);
 }
 
 TEST(FactoryTest, MultiplyingNeg){
         Factory test;
         char *args[] = {"-4", "*", "4"};
-        Base *op = test.parse(args, 3);
+        Base *Op = test.parse(args, 3);
         EXPECT_DOUBLE_EQ(op->evaluate(), -16);
 }
 
 TEST(FactoryTest, DividingNeg){
         Factory test;
         char *args[] = {"50", "/", "-5"};
-        Base *op = test.parse(args, 3);
+        Base *Op = test.parse(args, 3);
         EXPECT_DOUBLE_EQ(op->evaluate(), -10);
 }
 
